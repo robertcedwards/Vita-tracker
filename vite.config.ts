08f69a8481f68cde12b25dfde7aeec686e2e11d6
Robert C Edwards <139775+robertcedwards@users.noreply.github.com>
2024-12-06 12:41:53 -0600

    removing middleware

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,2 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { setupApiMiddleware } from './src/server/middleware';
-
-export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'configure-server',
-      configureServer(server) {
-        server.middlewares.use(setupApiMiddleware(server));
-      },
-    },
-  ],
-});
